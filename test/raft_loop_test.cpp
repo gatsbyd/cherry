bac2ba606e212ae85a23fa42e494fff909c424e1
root <1240184197@qq.com>
2019-11-14 10:41:58 +0800

    cause:trivial commit

--- test/raft_loop_test.cpp
+++ test/raft_loop_test.cpp
@@ -9,6 +9,7 @@ int main() {
 	Singleton<Logger>::getInstance()->addAppender("console", LogAppender::ptr(new ConsoleAppender()));
 
 	Scheduler scheduler;
+	scheduler.startAsync();
 	std::vector<rpc::RpcClient::Ptr> peers;
 	cherry::Raft raft(peers, 0, IpAddress("127.0.0.1", 5000), &scheduler);
 	return 0;
